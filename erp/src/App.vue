48af211b7998d2460d67fa88964901655d566f69
RickZzDev <rick_teloon@hotmail.com>
2020-02-09 22:58:44 -0300

    subindo front com consumo de api

---
+++ erp/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Caixa/>
+  </div>
+</template>
+
+<script>
+
+
+
+import Caixa from './components/caixa'
+
+export default {
+  name: 'App',
+  components: {
+      Caixa,
+  }
+}
+</script>
+
+<style>
+
+/* @import 'assets/_bootstrap.scss';
+@import url('_variables.scss'); */
+
+body{
+  background: rgb(2,0,36);
+background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 53%, rgba(0,212,255,1) 100%);
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ erp/src/components/caixa.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container col-xl-5 py-4">
+      <div class="row px-5">
+          <h5>Nome</h5>
+          <input type="text" id="nome"   class="form-control"  aria-describedby="emailHelp" placeholder="Nome">
+      </div>
+      <div class="row px-5 mt-3">
+          <h5>CNPJ</h5>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Cnpj">
+      </div>
+      <div class="row px-5 mt-3">
+          <h5>Senha</h5>
+          <input type="email" class="form-control" id="senha" aria-describedby="emailHelp" placeholder="Senha">
+      </div>
+      <div class="row px-5">
+          <button type="button" v-on:click='getEmpresas()' class="btn btn-primary mt-3 ml-auto">Primary</button>
+      </div>
+   </div>
+</template>
+
+<script>
+
+
+
+export default {
+    name:'caixa',
+    methods:{
+
+         getEmpresas(){
+             let nome = document.getElementById('nome').value;
+             let senha =  document.getElementById('senha').value
+             const dadosRegistro = {
+                nome: nome,
+                senha:senha                
+             }
+
+
+            let url = ('http://127.0.0.1:8000/empresas/')
+
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nome: nome,
+                    senha:senha  
+                })
+            }).then(resposta=>resposta.json()).then(data=>console.log(data));
+
+            
+            console.log(dadosRegistro)
+            
+        }
+
+        
+    }
+}
+
+ 
+</script>
+
+<style scoped>
+   
+
+    .container{
+        background-color: rgb(221, 218, 218);
+        border-radius: 10px;
+    }
+</style>
